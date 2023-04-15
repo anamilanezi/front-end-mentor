7ec86d5aff177b60ff9065317ac5ec6641430e90
Ana Milanezi <anamilanezi@gmail.com>
2023-04-15 18:28:52 -0300

    toggle answer visibility when clicking on active question

--- faq-accordion/script/script.js
+++ faq-accordion/script/script.js
@@ -1,17 +1,47 @@
-const questions = document.querySelectorAll('.faq-question-box')
+const questionBoxes = document.querySelectorAll('.faq-question-box')
 
-console.log(questions)
 
-questions.forEach(question => 
-    question.addEventListener('click', () => {
-        questions.forEach(q => {
-            q.querySelector('.faq-question').classList.remove('active');
-            q.querySelector('.faq-arrow').classList.remove('up');
-            q.nextElementSibling.classList.add('hidden');
-        });
-        question.querySelector('.faq-question').classList.toggle('active');
-        question.querySelector('.faq-arrow').classList.toggle('up');
-        question.nextElementSibling.classList.toggle('hidden');
-        console.log("Finished")
+
+// questions.forEach(question => 
+//     question.addEventListener('click', () => {
+//         questions.forEach(q => {
+//             q.querySelector('.faq-question').classList.remove('active');
+//             q.querySelector('.faq-arrow').classList.remove('up');
+//             q.nextElementSibling.classList.add('hidden');
+//         });
+//         question.querySelector('.faq-question').classList.toggle('active');
+//         question.querySelector('.faq-arrow').classList.toggle('up');
+//         question.nextElementSibling.classList.toggle('hidden');
+//         console.log("Finished")
+//     }
+//     ))
+
+questionBoxes.forEach(questionBox =>
+    questionBox.addEventListener('click', () => {
+
+        let question = questionBox.querySelector('.faq-question').classList;
+        let arrow = questionBox.querySelector('.faq-arrow').classList;
+        let answer = questionBox.nextElementSibling.classList;
+
+        if (question.contains('active')) {
+            questionBox.querySelector('.faq-question').classList.toggle('active');
+            questionBox.querySelector('.faq-arrow').classList.toggle('up');
+            questionBox.nextElementSibling.classList.toggle('hidden');
+            console.log("Finished")
+        } else {
+            questionBoxes.forEach(q => {
+                q.querySelector('.faq-question').classList.remove('active');
+                q.querySelector('.faq-arrow').classList.remove('up');
+                q.nextElementSibling.classList.add('hidden');
+            });
+            questionBox.querySelector('.faq-question').classList.toggle('active');
+            questionBox.querySelector('.faq-arrow').classList.toggle('up');
+            questionBox.nextElementSibling.classList.toggle('hidden');
+            console.log("Finished")
+        }
+
+
+
     }
     ))
+
