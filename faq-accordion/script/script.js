3f61817dbb88c8378af3f6d58adad79df959e3c9
Ana Milanezi <anamilanezi@gmail.com>
2023-04-16 13:55:23 -0300

    change question html to details and summary tags

--- faq-accordion/script/script.js
+++ faq-accordion/script/script.js
@@ -1,4 +1,4 @@
-const questionBoxes = document.querySelectorAll('.faq-question-box')
+// const questionBoxes = document.querySelectorAll('.faq-question-box')
 
 
 
@@ -16,29 +16,39 @@ const questionBoxes = document.querySelectorAll('.faq-question-box')
 //     }
 //     ))
 
-questionBoxes.forEach(questionBox =>
-    questionBox.addEventListener('click', () => {
+// questionBoxes.forEach(questionBox =>
+//     questionBox.addEventListener('click', () => {
 
-        let question = questionBox.querySelector('.faq-question').classList;
-        let arrow = questionBox.querySelector('.faq-arrow').classList;
-        let answer = questionBox.nextElementSibling.classList;
+//         let question = questionBox.querySelector('.faq-question').classList;
+//         let arrow = questionBox.querySelector('.faq-arrow').classList;
+//         let answer = questionBox.nextElementSibling.classList;
 
-        if (question.contains('active')) {
-            questionBox.querySelector('.faq-question').classList.toggle('active');
-            questionBox.querySelector('.faq-arrow').classList.toggle('up');
-            questionBox.nextElementSibling.classList.toggle('hidden');
-            console.log("Finished")
-        } else {
-            questionBoxes.forEach(q => {
-                q.querySelector('.faq-question').classList.remove('active');
-                q.querySelector('.faq-arrow').classList.remove('up');
-                q.nextElementSibling.classList.add('hidden');
-            });
-            questionBox.querySelector('.faq-question').classList.toggle('active');
-            questionBox.querySelector('.faq-arrow').classList.toggle('up');
-            questionBox.nextElementSibling.classList.toggle('hidden');
-            console.log("Finished")
-        }
-    }
-    ))
+//         if (question.contains('active')) {
+//             questionBox.querySelector('.faq-question').classList.toggle('active');
+//             questionBox.querySelector('.faq-arrow').classList.toggle('up');
+//             questionBox.nextElementSibling.classList.toggle('hidden');
+//             console.log("Finished")
+//         } else {
+//             questionBoxes.forEach(q => {
+//                 q.querySelector('.faq-question').classList.remove('active');
+//                 q.querySelector('.faq-arrow').classList.remove('up');
+//                 q.nextElementSibling.classList.add('hidden');
+//             });
+//             questionBox.querySelector('.faq-question').classList.toggle('active');
+//             questionBox.querySelector('.faq-arrow').classList.toggle('up');
+//             questionBox.nextElementSibling.classList.toggle('hidden');
+//             console.log("Finished")
+//         }
+//     }
+//     ))
 
+const questionBoxes = document.querySelectorAll('.faq-group')
+
+questionBoxes.forEach(q => {
+    q.addEventListener('click', (e) => {
+        if (e.target.parentElement.open == true) return e.target.parentElement = false
+        questionBoxes.forEach(element => element.parentElement.open = false)
+
+            e.target.parentElement.open = false
+    })
+})
