d0a66f86ab986a8841155b2a6ff864b9e7e68880
Ana Milanezi <anamilanezi@gmail.com>
2023-04-16 14:36:52 -0300

    change script to use open attribute

--- faq-accordion/script/script.js
+++ faq-accordion/script/script.js
@@ -1,54 +1,14 @@
-// const questionBoxes = document.querySelectorAll('.faq-question-box')
+// Fetch all the details element.
+const details = document.querySelectorAll("details");
 
-
-
-// questions.forEach(question => 
-//     question.addEventListener('click', () => {
-//         questions.forEach(q => {
-//             q.querySelector('.faq-question').classList.remove('active');
-//             q.querySelector('.faq-arrow').classList.remove('up');
-//             q.nextElementSibling.classList.add('hidden');
-//         });
-//         question.querySelector('.faq-question').classList.toggle('active');
-//         question.querySelector('.faq-arrow').classList.toggle('up');
-//         question.nextElementSibling.classList.toggle('hidden');
-//         console.log("Finished")
-//     }
-//     ))
-
-// questionBoxes.forEach(questionBox =>
-//     questionBox.addEventListener('click', () => {
-
-//         let question = questionBox.querySelector('.faq-question').classList;
-//         let arrow = questionBox.querySelector('.faq-arrow').classList;
-//         let answer = questionBox.nextElementSibling.classList;
-
-//         if (question.contains('active')) {
-//             questionBox.querySelector('.faq-question').classList.toggle('active');
-//             questionBox.querySelector('.faq-arrow').classList.toggle('up');
-//             questionBox.nextElementSibling.classList.toggle('hidden');
-//             console.log("Finished")
-//         } else {
-//             questionBoxes.forEach(q => {
-//                 q.querySelector('.faq-question').classList.remove('active');
-//                 q.querySelector('.faq-arrow').classList.remove('up');
-//                 q.nextElementSibling.classList.add('hidden');
-//             });
-//             questionBox.querySelector('.faq-question').classList.toggle('active');
-//             questionBox.querySelector('.faq-arrow').classList.toggle('up');
-//             questionBox.nextElementSibling.classList.toggle('hidden');
-//             console.log("Finished")
-//         }
-//     }
-//     ))
-
-const questionBoxes = document.querySelectorAll('.faq-group')
-
-questionBoxes.forEach(q => {
-    q.addEventListener('click', (e) => {
-        if (e.target.parentElement.open == true) return e.target.parentElement = false
-        questionBoxes.forEach(element => element.parentElement.open = false)
-
-            e.target.parentElement.open = false
-    })
-})
+// Add the onclick listeners.
+details.forEach((targetDetail) => {
+  targetDetail.addEventListener("click", () => {
+    // Close all the details that are not targetDetail.
+    details.forEach((detail) => {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
