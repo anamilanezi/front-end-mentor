44f640648485678e718b6e76d7406e40f1bd9eb4
Ana Milanezi <anamilanezi@gmail.com>
2023-04-18 14:38:49 -0300

    add footer attribution, change script to use forEach when accessing values

--- tip-calculator-app/script/script.js
+++ tip-calculator-app/script/script.js
@@ -23,11 +23,9 @@ tipButtons.forEach((btnActive, i) => {
             btnDisabled.classList.remove('active')
         })
 
-
         // Will only add active class to predifined tip buttons
         if (i <= 4) {
             btnActive.classList.add('active')
-            console.log(btnActive.value)
             calculateTip(btnActive.value, billTotal.value, numOfPeople.value)
 
         }
@@ -35,31 +33,29 @@ tipButtons.forEach((btnActive, i) => {
 })
 
 
-// tipCustom.addEventListener('input', () => {
-//     calculateTip(tipCustom.value, billTotal.value, numOfPeople.value)
-// })
-
-
 inputValues.forEach((input) => {
     input.addEventListener('input', () => {
         let btnActive = document.querySelector('.active')
-        if (tipCustom.value === "" && btnActive != null) {
-            selectedTip = btnActive.value
-        } else {
-            selectedTip = tipCustom.value
-        }
-        calculateTip(selectedTip, billTotal.value, numOfPeople.value)
 
+        if (tipCustom.value === "" && btnActive != null) {
+            selectedTip = btnActive
+        } else {
+            selectedTip = tipCustom
+        }
+        calculateTip(selectedTip.value, billTotal.value, numOfPeople.value)
     })
 })
+
 
 reset.addEventListener('click', () => {
     tipButtons.forEach(btnDisabled => {
         btnDisabled.classList.remove('active')
     })
-    selectedTip.value = "";
-    billTotal.value = "";
-    numOfPeople.value = "";
+
+    inputValues.forEach(input => {
+        input.value = ""
+    })
+
     reset.disabled = true;
     tipResult.innerHTML = "$0.00"
     totalResult.innerHTML = "$0.00"
@@ -67,12 +63,13 @@ reset.addEventListener('click', () => {
 
 
 
-// Fazer uma função construtora, que muda tip 
+// Function to calculate tip
 const calculateTip = (tip, total, num) => {
 
     if (num == 0 || num == "") {
         document.querySelector('.error-msg').classList.remove('hidden')
         numOfPeople.classList.add('error')
+        return
     } else {
         document.querySelector('.error-msg').classList.add('hidden')
         numOfPeople.classList.remove('error')
@@ -86,15 +83,11 @@ const calculateTip = (tip, total, num) => {
         totalPerson = roundMoney(totalPerson)
         tipPerson = roundMoney(tipPerson)
 
-        console.log(tipPerson)
-        
         totalResult.innerHTML = `$${totalPerson}`;
         tipResult.innerHTML = `$${tipPerson}`;
         
         reset.disabled = false;
     }
-    
-
 }
 
 const roundMoney = num => (Math.round((num + Number.EPSILON) * 100) / 100).toFixed(2)
