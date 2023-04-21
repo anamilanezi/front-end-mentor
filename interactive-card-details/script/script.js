785411f874f08984d9425d662bd8bcda96ac8412
Ana Milanezi <anamilanezi@gmail.com>
2023-04-21 14:41:23 -0300

    add script to change card detail while typing

--- interactive-card-details/script/script.js
+++ interactive-card-details/script/script.js
@@ -1,31 +1,64 @@
 // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_oninput
 
-const cardNum = document.querySelector('.card-number')
 
-const numInput = document.querySelector('.card-num-input')
+const cardName = document.querySelector('.front__info-name');
+const formName = document.getElementById('form__name');
 
-const cardGroup = document.querySelectorAll('span')
+const cardNum = document.querySelector('.front__info-num');
+const formNum = document.getElementById("form__num");
 
-let g1 = document.querySelector('.g1')
+const cardVal = document.querySelector('.front__info-val')
+const formValMonth = document.getElementById('form__month');
+const formValYear = document.getElementById('form__year');
 
-let typedNum = [];
+const cardCvc = document.querySelector('.back__cvc')
+const formCvc = document.getElementById('form__cvc')
 
-numInput.addEventListener('input', (e) => {
-    let text = numInput.value;
+const btn = document.querySelector('.btn')
 
-    for (let i = 0; i <= 16; i += 4) {
-        if (text.length <= 4) {
-            cardGroup[3].innerHTML = text
-        } else if (text.length <= 8) {
-            cardGroup[2].innerHTML = text
-        } else if (text.length <= 12) {
-            cardGroup[1].innerHTML = text
-        } else if (text.length <= 16) {
-            cardGroup[0].innerHTML = text
-        } else {
-            
-        }
-    }
-    // cardNum.innerHTML = text;
+const warningName = document.querySelector('.form__warning.name')
+const warningNum = document.querySelector('.form__warning.num')
+const warningDate = document.querySelector('.form__warning.date')
+const warningCvc = document.querySelector('.form__warning.cvc')
+
+
+formName.addEventListener('input', () => {
+    let text = formName.value;
+    cardName.textContent = text;
 })
 
+
+formNum.addEventListener('input', () => {
+    let text = formNum.value;
+
+    let isNum = /^[0-9 ]+$/.test(text);
+    if (!isNum) {
+        formNum.classList.add('error')
+        warningNum.classList.remove('hidden')
+    } else {
+        formNum.classList.remove('error')
+        warningNum.classList.add('hidden')
+        cardNum.textContent = text
+    }
+})
+
+
+
+
+btn.addEventListener('click', () => {
+    if (isEmpty(formValMonth.value) || isEmpty(formValYear.value) || isEmpty(formName.value)) {
+        warningDate.classList.remove('hidden');
+    }
+});
+
+
+const isEmpty = (formValue) => {
+    if (typeof(formValue) === string) {
+        return formValue === ""
+    }
+
+    if (typeof(formValue) === number) {
+        return formValue === ""
+    }
+
+}
